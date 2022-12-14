1320a208857a51d5571397eff37a76d96ace4b13
maximyarochevsky <maximyarochevskygithub@gmail.com>
2022-12-14 18:34:52 +0300

    Добавление возможности чтения содержимого файла конфигурации

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
 
-app.MapGet("/", () => "Hello World!");
+namespace WebStore
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
 
-app.Run();
+            // Загрузка информации из файла конфигурации
+            // Создание переменной конфигурации
+
+            var configuration = app.Configuration;
+
+            //Переменная параметра
+
+            var greetings = configuration["CustomGreetings"];
+
+            app.MapGet("/", () => greetings);
+
+            app.Run();
+
+        }
+    }
+}
+
